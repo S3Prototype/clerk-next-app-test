7276a08d2f4a4731b6121e66ced530e675e6cb4c
S3Prototype <liuqahs15@gmail.com>
2024-05-10 13:22:19 -0400

    Updates 2

--- app/image-optimization/page.tsx
+++ app/image-optimization/page.tsx
@@ -1,4 +1,4 @@
-import { currentUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function ImageOptimization() {
   const user = await currentUser();
